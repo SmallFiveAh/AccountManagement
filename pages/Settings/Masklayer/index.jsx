3ce0fc82f4c1cd84057bb6eb61f593925aea4bc2
SmallDuck <108576501+qip389@users.noreply.github.com>
2025-09-29 22:32:20 +0800

    feat(Settings/Masklayer): 更新遮罩层组件视频背景路径并添加样式文件
    - 导入 useState hook 和 CSS 样式文件
    - 修改视频背景路径为本地资源文件
    - 添加组件样式文件 index.css

--- pages/Settings/Masklayer/index.jsx
+++ pages/Settings/Masklayer/index.jsx
@@ -1,11 +1,12 @@
-import {  } from 'react';
+import { useState } from 'react'
+import './index.css';
 
 function Masklayer() {
   return (
     <div className="home-wallpaper h-full w-full">
         <video 
             className="h-full w-full object-cover" 
-            src="" 
+            src="../resource/video/background.mp4" 
             autoPlay 
             loop 
             muted
