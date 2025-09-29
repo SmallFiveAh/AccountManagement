3006b3a6cbdfa6389dea3103af96e4523d3bc5fe
SmallDuck <108576501+qip389@users.noreply.github.com>
2025-09-29 22:31:54 +0800

    feat(Settings): 替换设置页面的默认内容为Masklayer组件
    将设置页面中的测试文本替换为Masklayer组件，以实现多页面入口的功能需求。

--- pages/Settings/index.jsx
+++ pages/Settings/index.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Masklayer from './Masklayer'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>
-      测试多页面入口1111111
-    </div>
+    <Masklayer />
   </StrictMode>,
 )
