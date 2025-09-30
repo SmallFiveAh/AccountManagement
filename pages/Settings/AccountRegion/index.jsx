65c28c7d7b1003efc2a4de2e425bf801b59568dd
SmallDuck <108576501+qip389@users.noreply.github.com>
2025-09-30 22:08:17 +0800

    feat(AccountRegion): 添加账号区域图标和文本显示
    在设置页面的账号区域中添加了图标容器和文本显示功能，
    包括48px的图标图片和"账号"文本标签，
    提升了用户界面的视觉效果和信息展示完整性。

--- pages/Settings/AccountRegion/index.jsx
+++ pages/Settings/AccountRegion/index.jsx
@@ -7,7 +7,16 @@ function AccountRegion() {
   return (
     <div className="main-container">
         <div className="header-container">
-            
+          <div className="account-container">
+              <div className="icon-container">
+                  <div className="icon-box">
+                      <img src="../public/resource/img/icon-48.png" title="账号" alt="1" />
+                      <div className="text-container">
+                          <samp>账号</samp>
+                      </div>
+                  </div>
+              </div>
+          </div>
         </div>
     </div>
   )
