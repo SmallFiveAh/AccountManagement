23a8a10c73ffe416459d9b77a9d6f5d7a91fc555
SmallDuck <108576501+qip389@users.noreply.github.com>
2025-09-30 23:07:37 +0800

    fix(AccountRegion): 移除多余的主容器包装元素
    移除了 AccountRegion 组件中不必要的 .main-container 包装层，
    使页面结构更加简洁，同时保持原有功能和样式不变。

--- pages/Settings/AccountRegion/index.jsx
+++ pages/Settings/AccountRegion/index.jsx
@@ -3,21 +3,18 @@ import './index.css'
 
 function AccountRegion() {
   const [videoSrc, setVideoSrc] = useState('')
-
   return (
-    <div className="main-container">
-        <div className="header-container">
-          <div className="account-container">
-              <div className="icon-container">
-                  <div className="icon-box">
-                      <img src="../public/resource/img/icon-48.png" title="账号" alt="1" />
-                      <div className="text-container">
-                          <samp>账号</samp>
-                      </div>
+    <div className="header-container">
+      <div className="account-container">
+          <div className="icon-container">
+              <div className="icon-box">
+                  <img src="../public/resource/img/icon-48.png" title="账号" alt="1" />
+                  <div className="text-container">
+                      <samp>账号</samp>
                   </div>
               </div>
           </div>
-        </div>
+      </div>
     </div>
   )
 }
