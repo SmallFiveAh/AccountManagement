56e2c9e897cd2bf936446df1ff0ebf88e214b6f2
SmallDuck <108576501+qip389@users.noreply.github.com>
2025-09-30 23:07:30 +0800

    feat(Settings): 添加配置组件并调整页面布局
    在设置页面中引入了新的配置组件，并将账户区域和配置组件包裹在主容器中，
    以改善页面布局结构。

--- pages/Settings/index.jsx
+++ pages/Settings/index.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Masklayer from './Masklayer'
 import AccountRegion from './AccountRegion';
+import Configuration from './Configuration';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Masklayer />
-    <AccountRegion />
+    <div className="main-container">
+      <AccountRegion />
+      <Configuration />
+    </div>
   </StrictMode>,
 )
